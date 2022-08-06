013be9c1704613c4d5e234928d4bb6bfea2ed770
rewebdev24 <rohitdhakad101@gmail.com>
2022-08-06 13:24:17 +0530

    about_animate2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { useState } from 'react'
-// import About from './Components/about/About'
-import About2 from './Components/about/About2'
+import About from './Components/about/About'
 import Contact from './Components/contact/Contact'
-// import Experience from './Components/experience/Experience'
-import Experience2 from './Components/experience/Experience2'
+import Experience from './Components/experience/Experience'
 import Footer from './Components/footer/Footer'
 import Header from './Components/header/Header'
 import Nav from './Components/nav/Nav'
@@ -14,10 +12,8 @@ function App() {
     <div className="App">
        <Header/>
       <Nav/>
-      {/* <About/> */}
-      {/* <Experience/> */}
-      <About2/>
-      <Experience2/>
+      <About/>
+      <Experience/>
       <Projects/>
       <Contact/>
       <Footer/>
