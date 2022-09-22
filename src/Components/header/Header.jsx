df08260df1b1e3a442a994fa7ba96170a4fb521d
rewebdev24 <rohitdhakad101@gmail.com>
2022-09-23 02:00:07 +0530

    vanta-js

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,13 +1,40 @@
 import React from "react";
+import { useState,useEffect,useRef } from "react";
 import CTA from "./CTA";
 import "./header.css";
+import FOG from 'vanta/dist/vanta.fog.min'
 const Header = () => {
+  const [vantaEffect, setVantaEffect] = useState(null)
+  const myRef = useRef(null)
+  useEffect(() => {
+    if (!vantaEffect) {
+      setVantaEffect(FOG({
+        el: myRef.current,
+        mouseControls: true,
+        touchControls: true,
+        gyroControls: false,
+        minHeight: 200.00,
+        minWidth: 200.00,
+        highlightColor: 0x313131,
+        midtoneColor: 0x49c849,
+        lowlightColor: 0x28282f,
+        baseColor: 0x2eca2e
+      }))
+    }
+    return () => {
+      if (vantaEffect) vantaEffect.destroy()
+    }
+  }, [vantaEffect])
+
+
 
   return (
+    
     <div>
-      <div className="clipath"></div>
+    <div ref={myRef} className="clipath"></div>
       <header id="Home">
         <div className="container header_container">
+          <img className="profile_photu" src="/assets/profile.gif" alt="" />
           <div className="header_content">
             <h4 id="hello">Hello I'm</h4>
             <h2 id="rohit">
